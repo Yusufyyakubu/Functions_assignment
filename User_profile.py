fd7936213ac673724843e16c90abbb768b890b6f
Yusufyyakubu <110755736+Yusufyyakubu@users.noreply.github.com>
2024-01-08 10:28:11 +0100

    Delete User_profile.py

--- User_profile.py
+++
@@ -1,7 +0,0 @@
-def build_profile(first_name, last_name, **additional_info):
-    profile = {}
-    profile['first_name'] = first_name
-    profile['last_name'] = last_name
-    for key, value in additional_info.items():
-        profile[key] = value
-    return profile
